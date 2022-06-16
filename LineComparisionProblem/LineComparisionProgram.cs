4a378034dbf63bb2e02305f8e2b74e27f4ab5bb0
Prateek Pai <paiprateek123@gmail.com>
2022-06-16 07:21:55 +0530

    Calculate the Lenght of Line

--- LineComparisionProblem/LineComparisionProgram.cs
+++ LineComparisionProblem/LineComparisionProgram.cs
@@ -10,7 +10,20 @@ namespace LineComparisionProblem
     {
         public static void CheckLineComparision()
         {
-            Console.WriteLine("Welcome to Line Comparision Problem");
+                Console.WriteLine("Enter First Point");
+                Console.WriteLine("X1=");
+                double x1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Y1=");
+                double y1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("........................");
+                Console.WriteLine("Enter Second Point");
+                Console.WriteLine("X2=");
+                double x2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Y2=");
+                double y2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("........................");
+                double LenghtofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                Console.WriteLine("Lenght of a Line is : {0}", LenghtofLine);
         }
     }
 }
